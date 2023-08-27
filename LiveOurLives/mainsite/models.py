8438964dd751b8c3a8374c92c322db842d79d406
Chris <chriswhoprograms@gmail.com>
2023-08-27 01:14:02 -0400

    more data modeling

--- LiveOurLives/mainsite/models.py
+++ LiveOurLives/mainsite/models.py
@@ -1,18 +1,21 @@
 from django.db import models
 
 # Create your models here.
-class Retorts(models.Model):
+class MinimizerRetorts(models.Model):
     shortName = models.CharField(max_length = 100)
     longName = models.CharField(max_length = 400)
 
-class Facts(models.Model):
+class ActualInformation(models.Model):
     headline = models.CharField(max_length = 500)
     exerpt = models.CharField(max_length = 500)
     typeOfFact = models.CharField(max_length = 100) #article, study, etc
     archiveLink = models.CharField(max_length = 2000)
     liveLink = models.CharField(max_length = 2000)
     
-class Categories(models.Model):
+    retorts = models.ManyToManyField(MinimizerRetorts)
+    categories = models.ManyToManyField(InfoCategories)
+    
+class InfoCategories(models.Model):
     categoryName = models.CharField(max_length = 100)
     retorts = models.ManyToManyField(Retorts)
     facts = models.ManyToManyField(Facts)
