dff5202af4af9257e1f16953511eb01caefb0e0e
Chris <chriswhoprograms@gmail.com>
2023-09-04 23:35:55 -0400

    Basic display of data

--- LiveOurLives/mainsite/views.py
+++ LiveOurLives/mainsite/views.py
@@ -2,21 +2,24 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import render
-from .models import MinimizerRetorts
+from .models import MinimizerRetorts, InfoCategories, ActualInformation
 
 
 # Create your views here.
 def index(request):
     retortsQueryOutput = MinimizerRetorts.objects.all();
     context = {"MinimizerRetorts": retortsQueryOutput,
-    "defaultRetort": retortsQueryOutput.first()
+    "defaultRetort": retortsQueryOutput.first(),
+    "retortResults": {}
     }
     
     if request.method == "POST":
-        print("we post")
         if request.POST.get("minimizer-dropdown"):
-            print(request.POST.get("minimizer-dropdown"))
+            chosenRetort = retortsQueryOutput.filter(shortName__exact = request.POST.get("minimizer-dropdown")).first()
+            
             #note to me: yes this is actually supposed to be a single =
-            context["defaultRetort"] = retortsQueryOutput.filter(shortName__exact = request.POST.get("minimizer-dropdown")).first()
+            context["defaultRetort"] = chosenRetort
+            
+            context["retortResults"] = ActualInformation.objects.filter(retorts__exact = chosenRetort)
     
     return render(request, 'mainsite/index.html', context)
