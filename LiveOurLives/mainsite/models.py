ca96469ca743057da10c57cc5a5c6d90827633a4
chris <chriswhoprograms@gmail.com>
2023-09-16 22:02:23 -0400

    Small changes

--- LiveOurLives/mainsite/models.py
+++ LiveOurLives/mainsite/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class MinimizerRetorts(models.Model):
     shortName = models.CharField(max_length = 100)
     longName = models.CharField(max_length = 400, blank = True)
-    display = models.BooleanField(default = False)
+    display = models.BooleanField(default = True)
 
     def __str__(self):
         return self.shortName
