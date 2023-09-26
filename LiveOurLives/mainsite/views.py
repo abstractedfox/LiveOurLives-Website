3322e8633e11463f1952faa0d209b3b7dd01519d
Chris <chriswhoprograms@gmail.com>
2023-09-26 19:30:47 -0400

    Adjusted layout & page header. Changed urls/views so a query can be initiated from the URL and not necessarily from a POST request

--- LiveOurLives/mainsite/views.py
+++ LiveOurLives/mainsite/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import MinimizerRetorts, InfoCategories, ActualInformation, ImageInfo
 import random
 
-# Create your views here.
-def index(request):
+# Create your views here.  
+
+def renderPage(request, chosenRetort):
     retortsQueryOutput = MinimizerRetorts.objects.all()
 
     context = {"MinimizerRetorts": retortsQueryOutput,
@@ -21,20 +22,26 @@ def index(request):
     #minimizerImages = ImageInfo.objects.all()
     jawns = list(ImageInfo.objects.all())
     context["minimizerImage"] = random.sample(jawns, 1)[0]
-    
-    if request.method == "POST":
-        if request.POST.get("minimizer-dropdown"):
-            chosenRetort = retortsQueryOutput.filter(shortName__exact = request.POST.get("minimizer-dropdown")).first()
-            
-            #note to me: yes this is actually supposed to be a single =
-            context["defaultRetort"] = chosenRetort
-            
-            context["retortResults"] = ActualInformation.objects.filter(retorts__exact = chosenRetort)
-    
+        
+    filteredResults = retortsQueryOutput.filter(shortName__exact = chosenRetort).first()
+    if chosenRetort != "" and filteredResults != None:
+        context["defaultRetort"] = filteredResults
+        context["retortResults"] = ActualInformation.objects.filter(retorts__exact = filteredResults)
     else:
         blankOption = retortsQueryOutput.filter(shortName__exact = "BLANK")
-        if blankOption.count() == 1:
+        if blankOption.first() != None:
             context["defaultRetort"] = blankOption.first()
 
-
     return render(request, 'mainsite/index.html', context)
+
+def index(request):
+    if request.method == "POST":
+        if request.POST.get("minimizer-dropdown"):
+            #return renderPage(request, request.POST.get("minimizer-dropdown"))
+            url = "/" + request.POST.get("minimizer-dropdown")
+            return redirect(url)
+
+    return renderPage(request, "")
+
+def indexSelection(request, selection):
+    return renderPage(request, selection)
